08ca1b5993d52588b389548a730fbf7ba3197871
Nikhil Massand <nikhil.massand@gmail.com>
2021-05-27 15:52:45 -0400

    working producer without committing the node modules

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+// import Consumer from './Consumer';
+import Producer from './Producer';
+
+function App() {
+  return(
+    <div>
+      <Producer />
+    </div>
+  )
+}
+
+export default App;
